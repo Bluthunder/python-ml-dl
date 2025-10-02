cae2a888af0ecb1908cb45a42708e33e601eaa56
kaushiktd <kaushiktd@gmail.com>
2025-10-02 20:49:58 +0530

    array

---
+++ Search/FirstAndLastPosition.py
@@ -0,0 +1,28 @@
+from typing import List
+
+def searchRange(nums: List[int], target: int) -> List[int]:
+
+    left = binary(nums, target, True)
+    right = binary(nums, target, False)
+
+    return[left, right]
+
+    def binary(nums: List[int], target:int, leftBias:bool)->int:
+
+        l, r = 0, len(nums) - 1
+        i = -1
+
+        while l < r:
+            m = (l+r)//2
+            if target > nums[m]:
+                l = m + 1
+            elif target < nums[m]:
+                r = m - 1
+            else:
+                i = m
+                if leftBias:
+                    r = m - 1
+                else:
+                    l = m + 1
+
+        return i
